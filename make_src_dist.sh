8f1b1a8ee15b5bb785ab3fbed225e7c6757fdea3
Nima Mousavi <mousavi@ucsd.edu>
2020-08-15 12:55:51 -0700

    Made make_src_dist more user friendly and self contained

--- make_src_dist.sh
+++ make_src_dist.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+# Create a new git tag before running this script 
+# git tag -a vX.Y.Z -m"message"
 die()
 {
     BASE=$(basename "$0")
     echo "$BASE error: $1" >&2
     exit 1
 }
-
+./reconf 
 ./configure || die "./configure failed"
 
 GITID=$(./config/git-version-gen .tarball-version) || die "Failed to get GIT ID"
