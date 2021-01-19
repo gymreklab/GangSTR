42c0e862df2ce158c64132a832a4f73d6e724ef5
Nima Mousavi <mousavi@ucsd.edu>
2021-01-19 13:26:55 -0800

    Removed filed from makefile era

--- test_profile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-valgrind --tool=callgrind \
-    ./src/GangSTR \
-    --bam tests/test.sorted.bam \
-    --ref /storage/resources/dbase/human/hg19/Homo_sapiens_assembly19.fasta \
-    --regions test.bed \
-    --out test
